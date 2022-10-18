dbac4d324e369f7be3fac3ddca09c3ff624d7d2a
codewithlaksh <dashlakshyaraj2006@gmail.com>
2022-10-18 21:55:47 +0530

    Change host

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const connectToMongo = require('./db');
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
 
 connectToMongo();
 
@@ -13,6 +14,7 @@ app.use((req, res, next) => {
     res.status(404).json({ status: 404, message: `Route not found! Please go to api/blogs.` });
 })
 
-app.listen(port, () => {
+
+app.listen(port, host, async () => {
     console.log(`Express api app listening on http://localhost:${port}`);
 })
